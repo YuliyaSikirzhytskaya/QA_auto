767073da1662c7c9c84dea75805125d91a4bc4cc
YuliyaSikirzhytskaya <YuliyaSikirzhytskaya@gmail.com>
2023-11-01 19:54:54 +0100

    CNNAutomation_part_2

---
+++ CNNAutomation/CNNHomePage.cs
@@ -0,0 +1,99 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace CNNAutomation
+{
+    internal class CNNHomePage
+    {
+        IWebDriver _webDriver;
+        const string HOME_URL = "https://edition.cnn.com/";
+
+        //const string LINK_TEXT_WORLD = "World";
+        //const string LINK_TEXT_TRAVEL = "Travel";
+        //const string LINK_TEXT_STYLE = "Style";
+        //const string LINK_TEXT_SPORTS = "Sports";
+        //const string LINK_TEXT_VIDEO = "Video";
+
+
+        public CNNHomePage(IWebDriver driver) 
+        {
+            _webDriver = driver;
+            _webDriver.Url = HOME_URL;
+
+            _webDriver.Manage().Window.Maximize();
+
+            IWebElement cookie = _webDriver.FindElement(By.Id("onetrust-accept-btn-handler"));
+            cookie.Click();
+        }
+
+        public bool CheckPageLink(string menuPointName)
+        {
+            //string linkText = String.Empty;
+            string keyWord = String.Empty;
+
+            if (menuPointName == "Sports")
+            {
+                keyWord = "sport";
+            }
+            else
+            {
+                keyWord = menuPointName.ToLower();
+            }
+
+            //switch (menuPointName) 
+            //{
+            //    case "World":
+            //        linkText = LINK_TEXT_WORLD;
+            //        break;
+            //    case "Travel":
+            //        linkText = LINK_TEXT_TRAVEL;
+            //        break;
+            //    case "Style":
+            //        linkText = LINK_TEXT_STYLE;
+            //        break;
+            //    case "Sport":
+            //        linkText = LINK_TEXT_SPORTS;
+            //        break;
+            //    case "Video":
+            //        linkText = LINK_TEXT_VIDEO;
+            //        break;
+            //    default:
+            //        throw new ArgumentException("No menu point");
+            //}
+
+            //if (menuPointName == "World")
+            //{
+            //    linkText = LINK_TEXT_WORLD;
+            //}
+            //else if (menuPointName == "Travel")
+            //{
+            //    linkText = LINK_TEXT_TRAVEL;
+            //}
+            //else if (menuPointName == "Style")
+            //{
+            //    linkText = LINK_TEXT_STYLE;
+            //}
+            //else if (menuPointName == "Sport") 
+            //{
+            //    linkText = LINK_TEXT_SPORTS;
+            //}
+            //else if (menuPointName == "Video")
+            //{
+            //    linkText = LINK_TEXT_VIDEO;
+            //}
+
+            IWebElement menuItem = _webDriver.FindElement(By.LinkText(menuPointName));
+
+            menuItem.Click();
+
+            string title = _webDriver.Title;
+            string url = _webDriver.Url;
+
+            //return to homePage
+            _webDriver.Url = HOME_URL;
+
+            return title.ToLower().Contains(keyWord) && url.Contains(keyWord);
+        }
+    }
+}
--- CNNAutomation/Program.cs
+++ CNNAutomation/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using CNNAutomation;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
@@ -6,49 +7,16 @@ using OpenQA.Selenium.Support.UI;
 using System.Xml.Linq;
 
 
-static void CheckPage(IWebDriver driver, string linkText, string keyword) 
-{
-
-
-    IWebElement menuItem = driver.FindElement(By.LinkText(linkText));
-
-    menuItem.Click();
-
-    string title = driver.Title;
-    Console.WriteLine("Page is opened: {0}", title.ToLower().Contains(keyword));
-
-    string url = driver.Url;
-    Console.WriteLine("Test case 2: {0}", url.Contains(keyword));
-
-    driver.Url = "https://edition.cnn.com/";
-}
-
 IWebDriver driver = new ChromeDriver();
 
-//WebDriverWait _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
-driver.Url = "https://edition.cnn.com/";
+CNNHomePage homePage = new CNNHomePage(driver);
 
-driver.Manage().Window.Maximize();
+bool isWorldPageDisplayed = homePage.CheckPageLink("World");
+bool isTravelPageDisplayed = homePage.CheckPageLink("Travel");
+bool isStylePageDisplayed = homePage.CheckPageLink("Style");
+bool isSportsPageDisplayed = homePage.CheckPageLink("Sports");
+bool isVideoPageDisplayed = homePage.CheckPageLink("Video");
 
-IWebElement cookie = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
-cookie.Click();
-
-CheckPage(driver, "World", "world");
-CheckPage(driver, "Travel", "travel");
-CheckPage(driver, "Style", "style");
-CheckPage(driver, "Sports", "sports");
-CheckPage(driver, "Video", "video");
-
-//IWebElement travelMenuItem = driver.FindElement(By.LinkText("Travel"));
-//travelMenuItem.Click();
-
-//title = driver.Title;
-//Console.WriteLine("Travel page is opened: {0}", title == "CNN Travel | Global Destinations, Tips & Video | CNN");
-
-//url = driver.Url;
-//Console.WriteLine("Test case 2: {0}",url.Contains("travel"));
-
-
+Console.WriteLine($"Test result: {isWorldPageDisplayed && isTravelPageDisplayed && isStylePageDisplayed && isSportsPageDisplayed && isVideoPageDisplayed}");
 
 driver.Close();
-Console.WriteLine("Hello, World!");
